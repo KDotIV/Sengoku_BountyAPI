d4da21490427aa75fd92565cfe9b01f4c849bfdf
K <kylebsmall32@gmail.com>
2022-08-19 13:24:46 -0400

    initial push for API

---
+++ Sengoku_BountyAPI/Controllers/BasicController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sengoku_BountyAPI.Controllers
+{
+    public class BasicController : ControllerBase
+    {
+        private readonly ILogger<BasicController> _logger;
+
+        public BasicController(ILogger<BasicController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
